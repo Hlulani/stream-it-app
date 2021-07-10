2d86d0a9e15730b0b0fafbfb8e71dbc6e2a08fb5
Hlulani Baloyi <hlullyr@gmail.com>
2021-07-11 00:05:32 +0200

    Added a search button

--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,23 @@
 
-import { HomeIcon, SearchIcon } from '@heroicons/react/outline'
+import { HomeIcon} from '@heroicons/react/outline'
+import { prototype } from 'form-data';
 import Image from "next/image"
 import logo from "../img/1.jpg"
 import HeaderItems from "./HeaderItems";
+import Searchbox from './Searchbox';
+import propTypes from 'prop-types'
 
 export default function Header() {
+
+    Header.propTypes = {
+        searchField: e.target.value
+    }
+
     return (
         <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
             <div className="flex flex-grow justify-evenly max-w-2xl">
                 <HeaderItems title="HOME" Icon={HomeIcon} />
-                <HeaderItems title="SEARCH" Icon={SearchIcon} />
+                <Searchbox placeholder="SEARCH" onChange={(e) => console.log(e.target.value)}/>
             </div>
             <Image
                 className="object-contain"
---
+++ components/Searchbox.js
@@ -0,0 +1,16 @@
+
+function Searchbox({ placeholder, onChange}) {
+    return (
+    <div className="inline-flex flex-col justify-center relative text-gray-500">
+    <div className="relative">
+        <input type="text" className="p-2 pl-8 rounded border border-gray-200 bg-gray-200 focus:bg-white focus:outline-none focus:ring-2 focus:ring-yellow-600 focus:border-transparent" placeholder="search..."
+         value="" onChange={onChange} />
+        <svg className="w-4 h-4 absolute left-2.5 top-3.5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"/>
+        </svg>
+    </div>
+    </div>
+    )
+}
+
+export default Searchbox
